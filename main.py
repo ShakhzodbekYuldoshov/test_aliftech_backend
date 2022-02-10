3a2e7061c39bc42df5d3006c26482cfb4f4fd2ea
ShakhzodbekYuldoshov <shahzodyoldoshov10@gmail.com>
2022-02-10 17:53:49 +0500

    /classify_gender/?queries... endpoint added

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
-from typing import Optional
-
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
@@ -10,7 +23,26 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/classify_gender/")
+def classify_gender(
+    age: int,
+    height_cm: float,
+    weight_kg: float,
+    body_fat_percent: float,
+    diastolic: float,
+    systolic: float,
+    grip_force: float,
+    sit_and_bend_forward_cm: float,
+    sit_ups_count: float,
+    broad_jump_cm: float
+):
+    '''
+    POST method used  for classifying gender
 
+    '''
+
+    return {"item_id": 'hello', "q": '1'}
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8000, debug=True)
